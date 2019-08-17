83ff85226c1dfefa0f5f2492f662c7480b15fbb5
JSONP <58239+jaysonjphillips@users.noreply.github.com>
2019-08-16 17:28:13 -0700

    fix contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,14 +31,14 @@ const Contact = (props) => (
                         <a href="#">contact@hospitalitytribe.com</a>
                     </div>
                 </section>
-                <section>
+                {/* <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
                         <span>(000) 000-0000 x12387</span>
                     </div>
-                </section>
-                <section>
+                </section> */}
+                {/* <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3>
@@ -46,7 +46,7 @@ const Contact = (props) => (
                         Nashville, TN 00000<br />
                         United States of America</span>
                     </div>
-                </section>
+                </section> */}
             </section>
         </div>
     </section>
